313d3f3aa9184c7b8b53a41bb6a4ba6887f3edf0
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2018-02-09 16:11:11 -0700

    Adjusted title so NuGet doesn't think it conflicts with the other version of this project.

--- src/Rhythm.Caching.Umbraco6/Properties/AssemblyInfo.cs
+++ src/Rhythm.Caching.Umbraco6/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rhythm.Caching.Umbraco")]
+[assembly: AssemblyTitle("Rhythm.Caching.Umbraco6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
